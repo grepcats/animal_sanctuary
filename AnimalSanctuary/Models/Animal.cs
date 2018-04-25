4e458b0e2a1cf0308fc8820f7367f4c387c0f66c
Kayla Ondracek and Stephanie Faber <kayla.renee@gmail.com and eyesicedover@gmail.com>
2018-04-25 09:36:14 -0700

    create project, add animal interface, add test project, add animal and vet classes

---
+++ AnimalSanctuary/Models/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimalSanctuary.Models
+{
+    [Table("Animals")]
+    public class Animal
+    {
+        [Key]
+        public int AnimalId { get; set; }
+        public string Species { get; set; }
+        public string Sex { get; set; }
+        public string HabitatType { get; set; }
+        public bool MedicalEmergency { get; set; }
+        public int VetId { get; set; }
+        public virtual Vet Vet { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## These are specs now
+A user should be able to see a list of all animals at the sanctuary.
+A user can view an individual animals details.
+A user should be able to see a list of all veterinarians at the sanctuary.
+A user can see a veterinarians details.
+A user should be able to add a new veterinarian.
+A user should be able to admit a new animal, linked with its veterinarian.
