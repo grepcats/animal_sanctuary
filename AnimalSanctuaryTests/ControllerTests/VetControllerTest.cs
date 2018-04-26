10ee5efd9e5a1f03b4da89eb1eff5f6f42d50af0
Kayla Ondracek and Stephanie Faber <kayla.renee@gmail.com and eyesicedover@gmail.com>
2018-04-26 10:10:41 -0700

    add tests for vet controller

--- AnimalSanctuaryTests/ControllerTests/VetControllerTest.cs
+++ AnimalSanctuaryTests/ControllerTests/VetControllerTest.cs
@@ -12,7 +12,7 @@ using System.Linq;
 namespace AnimalSanctuaryTests.ControllerTests
 {
     [TestClass]
-    class VetControllerTest
+    public class VetControllerTest
     {
         Mock<IVetRepository> mock = new Mock<IVetRepository>();
         private void DbSetup()
@@ -20,7 +20,7 @@ namespace AnimalSanctuaryTests.ControllerTests
             mock.Setup(m => m.Vets).Returns(new Vet[]
             {
                 new Vet {VetId = 1, Name = "Jeremiah Fink", Specialty = "Horsebonologist"},
-                new Vet {VetId = 1, Name = "Jeremiah Fink", Specialty = "Holder of Eldritch Frog Knowledge"}
+                new Vet {VetId = 1, Name = "Bilbo Baggins", Specialty = "Holder of Eldritch Frog Knowledge"}
             }.AsQueryable());
         }
 
@@ -36,6 +36,80 @@ namespace AnimalSanctuaryTests.ControllerTests
 
             //Assert
             Assert.IsInstanceOfType(result, typeof(List<Vet>));
+        }
+
+        [TestMethod]
+        public void Mock_VetGetViewResultIndex_ActionResult()
+        {
+            //Arrange
+            DbSetup();
+            VetController controller = new VetController(mock.Object);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Mock_VetIndexModelContainsAnimals_Collection()
+        {
+            //arrange
+            DbSetup();
+            VetController controller = new VetController(mock.Object);
+            Vet testVet = new Vet();
+            testVet.Name = "Jeremiah Fink";
+            testVet.Specialty = "Horsebonologist";
+            testVet.VetId = 1;
+
+            //act
+            ViewResult indexView = controller.Index() as ViewResult;
+            List<Vet> collection = indexView.ViewData.Model as List<Vet>;
+
+            //assert
+            CollectionAssert.Contains(collection, testVet);
+        }
+
+        [TestMethod]
+        public void Mock_VetPostViewResultCreate_ViewResult()
+        {
+            //Arrange
+            Vet testVet = new Vet
+            {
+                VetId = 1,
+                Name = "Jeremiah Fink",
+                Specialty = "Horsebonologist"
+            };
+            DbSetup();
+            VetController controller = new VetController(mock.Object);
+
+            //Act
+            var resultView = controller.Create(testVet);
+            Console.WriteLine(resultView.GetType());
+            var type = resultView.GetType();
+
+            //Assert
+            Assert.IsInstanceOfType(resultView, typeof(RedirectToActionResult));
+        }
+
+        [TestMethod]
+        public void Mock_VetGetDetails_ReturnsView()
+        {
+            // Arrange
+            Vet testVet = new Vet();
+            testVet.VetId = 1;
+            testVet.Name = "Jeremiah Fink";
+            testVet.Specialty = "Horsebonologist";
+            DbSetup();
+            VetController controller = new VetController(mock.Object);
+
+            //Act
+            var resultView = controller.Details(testVet.VetId) as ViewResult;
+            var model = resultView.ViewData.Model as Vet;
+
+            //Assert
+            Assert.IsInstanceOfType(resultView, typeof(ViewResult));
         }
     }
 }
